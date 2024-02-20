29ad4c122cc3d1190b4ae01e2092da2a7f8b3684
MohamedAhmed365 <mohamedainmc@gmail.com>
2024-02-20 10:59:37 +0200

    prefinal version before "Delete All"

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let b = document.getElementsByClassName("addButton")[0];
+let a = document.getElementsByClassName("add")[0];
 let i = document.getElementsByClassName("input")[0];
 let ul = document.getElementsByClassName("ul")[0];
 
@@ -7,7 +8,7 @@ let list = [];
 b.addEventListener("click", function () {
     if (document.getElementsByClassName("input")[0].value !== "") {
         let li = document.createElement("li");
-        
+        li.setAttribute("editing", "false");
         let doName = document.createElement("div");
         doName.classList.add("doName");
         doName.innerHTML = i.value;
@@ -16,7 +17,7 @@ b.addEventListener("click", function () {
         let star = document.createElement("button");
         star.classList.add("unstar");
         star.addEventListener("click", function () {
-            console.log(this.classList[0]);
+            // console.log(this.classList[0]);
             if (this.classList.contains("unstar")) {
                 this.classList.remove("unstar");
                 this.classList.add("star");
@@ -25,12 +26,12 @@ b.addEventListener("click", function () {
                 this.classList.add("unstar");
             }
             // this.style.backgroundImage.url = "../img/star.png";
-            console.log(this.classList[0]);
+            // console.log(this.classList[0]);
             // console.log("star");
             rewrite();
         });
         li.appendChild(star);
-        
+
         let done = document.createElement("input");
         done.setAttribute("type", "checkbox");
         done.classList.add("done");
@@ -38,17 +39,65 @@ b.addEventListener("click", function () {
             rewrite();
         });
         li.appendChild(done);
-        
-        let rem = document.createElement("rem");
+
+        let rem = document.createElement("button");
         rem.classList.add("rem");
         rem.addEventListener("click", function () {
             hideParent(this);
             rewrite();
         });
         li.appendChild(rem);
+
+        let edit = document.createElement("button");
+        edit.classList.add("edit");
+        edit.addEventListener("click", function () {
+            b.style.display = "none";
+            i.focus();
+            i.style.marginLeft = "7%";
+            this.parentElement.setAttribute("editing", "true");
+            editing()
+            let olde = this;
+            let old = this.parentElement.childNodes[0].innerHTML
+            i.value = old;
+
+            let ok = document.createElement("button");
+            ok.classList.add("ok");
+            ok.addEventListener("click", function () {
+                if (i.value !== "") {
+                    olde.parentElement.childNodes[0].innerHTML = i.value;
+                    olde.parentElement.setAttribute("editing", "false");
+                    editing2()
+                    i.style.marginLeft = "10%";
+                    i.value = "";
+                    b.style.display = "inline-block";
+                    ok.style.display = "none";
+                    can.style.display = "none";
+                } else {
+                    i.setAttribute("placeholder", "empty input");
+                }
+            });
+
+            let can = document.createElement("button");
+            can.classList.add("can");
+            can.addEventListener("click", function () {
+                olde.parentElement.setAttribute("editing", "false");
+                editing2()
+                i.style.marginLeft = "10%";
+                i.value = "";
+                b.style.display = "inline-block";
+                ok.style.display = "none";
+                can.style.display = "none";
+            });
+            a.appendChild(ok);
+            a.appendChild(can);
+        });
+        li.appendChild(edit);
         
-        
-        
+        let wait = document.createElement("button");
+        wait.classList.add("wait");
+        wait.style.display = "none";
+        li.appendChild(wait);
+
         // li.innerHTML = `
         // <div class="doName">
         //             ${i.value}
@@ -79,24 +128,24 @@ i.addEventListener("focus", function () {
 
 function rewrite() {
     let newList2 = newList();
-    console.log(newList2);
+    // console.log(newList2);
     ul.innerHTML = '';
     newList2.forEach(element => {
         ul.appendChild(element);
     });
 }
 
-function newList()  {
+function newList() {
     let newList = [];
     let clist = ul.childNodes;
-    console.log(ul.childNodes);
+    // console.log(ul.childNodes);
     clist.forEach(element => {
         if (element.style.display !== 'none') {
-            console.log("1")
+            // console.log("1")
             if (element.childNodes[1].classList.contains("star")) {
-                console.log("2.1")
-                if (! element.childNodes[2].checked) {
-                    console.log("2.2")
+                // console.log("2.1")
+                if (!element.childNodes[2].checked) {
+                    // console.log("2.2")
                     newList.push(element);
                 }
             }
@@ -104,11 +153,11 @@ function newList()  {
     });
     clist.forEach(element => {
         if (element.style.display !== 'none') {
-            console.log("3")
+            // console.log("3")
             if (element.childNodes[1].classList.contains("unstar")) {
-                console.log("4")
-                if (! element.childNodes[2].checked) {
-                    console.log("5")
+                // console.log("4")
+                if (!element.childNodes[2].checked) {
+                    // console.log("5")
                     newList.push(element);
                 }
             }
@@ -116,15 +165,37 @@ function newList()  {
     });
     clist.forEach(element => {
         if (element.style.display !== 'none') {
-            console.log("6")
+            // console.log("6")
             if (element.childNodes[1].classList.contains("unstar")) {
-                console.log("7")
-                if ( element.childNodes[2].checked) {
-                    console.log("8")
+                // console.log("7")
+                if (element.childNodes[2].checked) {
+                    // console.log("8")
                     newList.push(element);
                 }
             }
         }
     });
     return newList;
+}
+
+function editing() {
+    let clist = ul.childNodes;
+    clist.forEach(element => {
+        if (element.getAttribute('editing') === "true") {
+            console.log("1")
+            element.childNodes[4].style.display = "none";
+            element.childNodes[5].style.display = "inline-block";
+        } else {
+            console.log(element.childNodes);
+            element.childNodes[4].style.display = "none";
+        }
+    });
+}
+
+function editing2() {
+    let clist = ul.childNodes;
+    clist.forEach(element => {
+        element.childNodes[4].style.display = "inline-block";
+        element.childNodes[5].style.display = "none";
+    });
 }
